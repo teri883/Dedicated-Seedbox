3b3ab00c756c7324c2f8d98c6d1402b55a944ebb
teri883 <154943177+teri883@users.noreply.github.com>
2024-01-22 12:21:46 +0600

    Update Tune.sh

--- Tune.sh
+++ Tune.sh
@@ -12,10 +12,10 @@ if [ $(id -u) -ne 0 ]; then
 fi
 
 ## Check Linux Distro
-distro_codename="$(source /etc/os-release && printf "%s" "${VERSION_CODENAME}")"
-if [[ $distro_codename != buster ]] && [[ $distro_codename != bullseye ]] ; then
-	warn_1; echo "Only Debian 10/11 is supported"; normal_4
-	exit 1
+ubuntu_version=$(lsb_release -r -s)
+if [[ $ubuntu_version != "18.04" ]]; then
+    warn_1; echo "Only Ubuntu 18.04 is supported"; normal_4
+    exit 1
 fi
 
 while true; do
@@ -25,21 +25,28 @@ while true; do
     do
         case $opt in
             "Deluge Tuning")
-                need_input; read -p "Enter username of your Deluge: " username
-                read -p "Cache Size (unit:MiB): " cache;
-                Cache_de=$(expr $cache \* 64)
-                Deluge_libtorrent; break
+                need_input; read -p "Enter the username of your Deluge: " username
+                read -p "Cache Size (unit: MiB): " cache
+                Cache_de=$((cache * 64))
+                Deluge_libtorrent
+                break
                 ;;
             "Tweaked BBR Install")
                 apt-get -qqy install sudo
                 Tweaked_BBR
-                normal_1; echo "Reboot for Tweaked BBR to take effect"; break
+                normal_1; echo "Reboot for Tweaked BBR to take effect"
+                break
                 ;;
             "System Tuning")
-                CPU_Tweaking; NIC_Tweaking; Network_Other_Tweaking; Scheduler_Tweaking; kernel_Tweaking; break
+                CPU_Tweaking
+                NIC_Tweaking
+                Network_Other_Tweaking
+                Scheduler_Tweaking
+                kernel_Tweaking
+                break
                 ;;
             "Configure Boot Script")
-                normal_1; echo "Start Configuing Boot Script"
+                normal_1; echo "Start Configuring Boot Script"
                 wget https://raw.githubusercontent.com/jerry048/Seedbox-Components/main/Miscellaneous/.boot-script.sh && chmod +x .boot-script.sh
                 cat << EOF > /etc/systemd/system/boot-script.service
 [Unit]
@@ -54,7 +61,8 @@ RemainAfterExit=true
 [Install]
 WantedBy=multi-user.target
 EOF
-                systemctl enable boot-script.service; break
+                systemctl enable boot-script.service
+                break
                 ;;
             *) warn_1; echo "Please choose a valid action";;
         esac
