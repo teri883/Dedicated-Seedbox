98e4ab86561dd07495b22e8986b5b6020fa3517b
teri883 <154943177+teri883@users.noreply.github.com>
2024-03-04 14:35:20 +0600

    Update Tune.sh

--- Tune.sh
+++ Tune.sh
@@ -1,37 +1,33 @@
-#!/bin/bash
+#!/bin/sh
 
 tput sgr0; clear
 
 ## Load text color settings
-wget -qO- https://raw.githubusercontent.com/jerry048/Seedbox-Components/main/Miscellaneous/tput.sh -O /tmp/tput.sh
-source /tmp/tput.sh
-rm -f /tmp/tput.sh
+source <(wget -qO- https://raw.githubusercontent.com/i-abc/Seedbox-Components/main/Miscellaneous/tput.sh)
 
 ## Check Root Privilege
-if [ "$(id -u)" -ne 0 ]; then 
+if [ $(id -u) -ne 0 ]; then 
     warn_1; echo  "This script needs root permission to run"; normal_4
     exit 1 
 fi
 
 ## Check Linux Distro
 distro_codename="$(source /etc/os-release && printf "%s" "${VERSION_CODENAME}")"
-if [[ $distro_codename != buster ]] && [[ $distro_codename != bullseye ]] && [[ $distro_codename != bookworm ]]; then
-    warn_1; echo "Only Debian 10/11/12 is supported"; normal_4
-    exit 1
+if [[ $distro_codename != buster ]] && [[ $distro_codename != bullseye ]] ; then
+	warn_1; echo "Only Debian 10/11 is supported"; normal_4
+	exit 1
 fi
 
 while true; do
-    wget -qO- https://raw.githubusercontent.com/jerry048/Seedbox-Components/main/tweaking.sh -O /tmp/tweaking.sh
-    source /tmp/tweaking.sh
-    rm -f /tmp/tweaking.sh
+    source <(wget -qO- https://raw.githubusercontent.com/i-abc/Seedbox-Components/main/tweaking.sh)
     normal_3; options=("Deluge Tuning" "Tweaked BBR Install" "System Tuning" "Configure Boot Script")
     select opt in "${options[@]}"
     do
         case $opt in
             "Deluge Tuning")
                 need_input; read -p "Enter username of your Deluge: " username
-                read -p "Cache Size (unit:MiB): " cache;
-                Cache_de=$(expr $cache \* 64)
+                read -p "Cache Size (unit:GiB): " cache;
+                Cache1=$(expr $cache \* 65536)
                 Deluge_libtorrent; break
                 ;;
             "Tweaked BBR Install")
@@ -43,22 +39,8 @@ while true; do
                 CPU_Tweaking; NIC_Tweaking; Network_Other_Tweaking; Scheduler_Tweaking; kernel_Tweaking; break
                 ;;
             "Configure Boot Script")
-                normal_1; echo "Start Configuing Boot Script"
-                wget https://raw.githubusercontent.com/jerry048/Seedbox-Components/main/Miscellaneous/.boot-script.sh -O ~/.boot-script.sh && chmod +x ~/.boot-script.sh
-                cat << EOF > /etc/systemd/system/boot-script.service
-[Unit]
-Description=boot-script
-After=network.target
-
-[Service]
-Type=simple
-ExecStart=/root/.boot-script.sh
-RemainAfterExit=true
-
-[Install]
-WantedBy=multi-user.target
-EOF
-                systemctl enable boot-script.service; break
+                source <(wget -qO- https://raw.githubusercontent.com/i-abc/Seedbox-Components/main/Miscellaneous/boot-script.sh)
+                boot_script; break
                 ;;
             *) warn_1; echo "Please choose a valid action";;
         esac
